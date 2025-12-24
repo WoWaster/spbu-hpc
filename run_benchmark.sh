4f9c59ca325b949eab4a212d35639028ba0efd3f
Nick Ponomarev <wowasterdev@gmail.com>
2025-12-24 13:49:17 +0300

    feat: add benchmarking script

---
+++ run_benchmark.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+
+MATRIX_SIZE=100
+BUILD_TYPE=Release
+WARMUP_COUNT=3
+ITER_COUNT=10
+
+CORE_COUNT=$(nproc)
+
+## Step 1. Build everything
+
+echo "Building project"
+
+cmake . -B build/ -D CMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake --build build/
+
+echo ""
+
+## Step 2. Benchmark OpenMP
+
+echo "Running benchmarks with OpenMP with n=${MATRIX_SIZE}"
+
+OMP_BINARY=./build/labA_01_omp
+
+for ((i=1; i < CORE_COUNT; i=$((2 * i)))) do
+    echo "Running with ${i} cores"
+    echo "Warming up..."
+    for ((j=0; j < WARMUP_COUNT; j++)) do
+        OMP_NUM_THREADS=$i $OMP_BINARY $MATRIX_SIZE &> /dev/null
+    done
+    echo "Measuring..."
+    for ((j=0; j < ITER_COUNT; j++)) do
+        OMP_NUM_THREADS=$i $OMP_BINARY $MATRIX_SIZE
+    done
+    echo ""
+done
+
+echo "Running with ${CORE_COUNT} cores"
+echo "Warming up..."
+for ((j=0; j < WARMUP_COUNT; j++)) do
+    OMP_NUM_THREADS=$CORE_COUNT $OMP_BINARY $MATRIX_SIZE &> /dev/null
+done
+echo "Measuring..."
+for ((j=0; j < ITER_COUNT; j++)) do
+    OMP_NUM_THREADS=$CORE_COUNT $OMP_BINARY $MATRIX_SIZE
+done
+echo ""
+echo ""
